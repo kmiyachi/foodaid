728af56ceae32acbcae033d88324adbe7c0b99ac
quiquemz <semendez@ucsd.edu>
2016-11-03 15:16:58 -0700

    sign-in v1

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+/**
+ * Created by quiquemz on 11/3/16.
+ */
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+    initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+    console.log("Javascript connected!");
+    signIn();
+}
+
+function signIn() {
+    var emailAddress = $("#inputEmail").val();
+    $(".btn-signin").click(function (e) {
+        e.preventDefault();
+        /*console.log(emailAddress);
+        if(isValidEmailAddress(emailAddress)) {
+            $(".form-signin").attr("action", "/customer/home").submit();
+        }*/
+        $(".form-signin").attr("action", "/customer/home").submit();
+    });
+}
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};

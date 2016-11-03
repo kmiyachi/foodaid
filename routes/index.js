1d3c81cb1ddacb6d6ace00ef6602052444700f7a
quiquemz <semendez@ucsd.edu>
2016-11-03 15:18:52 -0700

    small changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 var users = require('../users.json');
 
 exports.view = function(req, res){
-    console.log(users.users[0]);
-    console.log("FDS");
     res.render('index');
 };
--- routes/retailer/options.js
+++ routes/retailer/options.js
@@ -1,6 +1,3 @@
 exports.view	=	function(req,	res){
     res.render('retailer/options');
 };
-/**
- * Created by kenmiyachi on 10/27/16.
- */

a9b113e7d14b23114195948c4f380556d42dad96
quiquemz <semendez@ucsd.edu>
2016-11-10 22:29:28 -0800

    added users

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var express = require('express');
 var firebase = require("firebase");
 var http = require('http');
 var path = require('path');
+var bodyParser = require('body-parser');
 var handlebars = require('express3-handlebars');
+var _ = require('lodash');
 
 // Routes
 var index = require('./routes/index');
@@ -23,6 +25,24 @@ var retailerInfo = require('./routes/retailer/info');
 var retailerVerification = require('./routes/retailer/verification');
 var retailerConfirmation = require('./routes/retailer/confirmation');
 
+var users = {
+  "usersList": [
+    {
+      "firstName": "Saul",
+      "lastName": "Mendez",
+      "type": "Customer",
+      "email": "semendez@ucsd.edu",
+      "password": "1234",
+      "id": 1,
+      "location": {
+        "address": "9450 Gilman Drive",
+        "lat": "32.872471",
+        "long": "-117.235228"
+      }
+    }
+  ]
+};
+
 // express variable
 var app = express();
 
@@ -32,12 +52,43 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all('/', function (req, res, next) {
+  console.log('Someone made a request!');
+  next();
+});
 
 // Add routes here
-app.get('/', index.view);
+app.get('/login', index.view);
+app.post('/login', function (req, res) {
+  var email = req.body.email;
+  var passw = req.body.password;
+  var user = _.find(users.usersList, {'email': email, 'password': passw});
+  if(user)
+    res.redirect('/customer/home');
+  else
+    res.redirect('/login');
+});
+
 app.get('/register', register.view);
+app.post('/register', function (req, res) {
+  users.usersList.push(
+      {
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "email": req.body.email,
+        "password": req.body.password,
+        "id": users.usersList.length
+      }
+  );
+  res.redirect('/login');
+});
 
-
+app.get('/users', function(req, res) {
+  res.send(users);
+});
 
 // Add customer routes
 app.get('/customer/home', customerHome.view);
@@ -49,7 +100,7 @@ app.get('/customer/map', map.view);
 app.get('/retailer/home', retailerHome.view);
 app.get('/retailer/info',retailerInfo.view);
 app.get('/retailer/options',retailerOptions.view);
-app.post('/retailer/verification',retailerVerification.view);
+app.get('/retailer/verification',retailerVerification.view);
 app.get('/retailer/confirmation', retailerConfirmation.view);
 
 

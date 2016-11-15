abb4fb547d9e5326f37242e03839193b9b813229
quiquemz <semendez@ucsd.edu>
2016-11-15 11:24:22 -0800

    added google analytics script

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 /* Module dependencies. */
 
-var express = require('express');
-var firebase = require("firebase");
-var http = require('http');
-var path = require('path');
 var bodyParser = require('body-parser');
+var express = require('express');
 var handlebars = require('express3-handlebars');
+var http = require('http');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var path = require('path');
 var _ = require('lodash');
 
-// Routes
+// Route Functions
 var index = require('./routes/index');
 var register = require('./routes/register');
 var settings = require('./routes/settings');
 var support = require('./routes/support');
 var paymentinfo = require('./routes/paymentinfo');
 
-//Customer Routes
+//Customer Routes Functions
 var customerHome = require('./routes/customer/home');
 var deal = require('./routes/customer/deal');
 var orderSummary = require('./routes/customer/order-summary');
 var map = require('./routes/customer/map');
 
-//Retailer Routes
+//Retailer Routes Functions
 var retailerHome = require('./routes/retailer/home');
 var retailerOptions = require('./routes/retailer/options');
 var retailerInfo = require('./routes/retailer/info');
@@ -44,51 +45,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.all('/', function (req, res, next) {
-  console.log('Someone made a request!');
-  next();
+//Re-routing to login st
+app.get('/', function (req, res) {
+  res.redirect('/login');
 });
 
 // Add routes here
-app.get('/', index.view);
+app.get('/login', index.view);
+app.post('/login', login);
 app.get('/register', register.view);
+app.post('/register', registerNewUser);
 app.get('/settings', settings.view);
 app.get('/support', support.view);
 app.get('/paymentinfo', paymentinfo.view);
 
-
-app.get('/login', index.view);
-app.post('/login', function (req, res) {
-  var email = req.body.email;
-  var passw = req.body.password;
-  var retailer = req.body.retailer === 'on';
-  var user = _.find(users.usersList, {'email': email, 'password': passw});
-  if(user && retailer)
-    res.redirect('/retailer/home');
-  else if(user)
-    res.redirect('/customer/home');
-  else
-    res.redirect('/login');
-});
-
-app.get('/register', register.view);
-app.post('/register', function (req, res) {
-  users.usersList.push(
-      {
-        "firstName": req.body.firstName,
-        "lastName": req.body.lastName,
-        "email": req.body.email,
-        "password": req.body.password,
-        "id": (users.usersList.length + 1)
-      }
-  );
-  res.redirect('/login');
-});
-
+// Routes to see current state of database
 app.get('/users', function(req, res) {
   res.send(users);
 });
-
 app.get('/offers', function(req, res) {
   res.send(offers);
 });
@@ -103,7 +77,46 @@ app.get('/customer/map', map.view);
 
 //Add retailer routes
 app.get('/retailer/home', retailerHome.view);
-app.post('/retailer/home', function (req, res) {
+app.post('/retailer/home', createNewOffer);
+app.get('/retailer/info',retailerInfo.view);
+app.get('/retailer/options',retailerOptions.view);
+app.get('/retailer/verification',retailerVerification.view);
+app.get('/retailer/confirmation', retailerConfirmation.view);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+//Helper functions
+function login(req, res) {
+  var email = req.body.email;
+  var passw = req.body.password;
+  var retailer = req.body.retailer === 'on';
+  var user = _.find(users.usersList, {'email': email, 'password': passw});
+  if(user && retailer)
+    res.redirect('/retailer/home');
+  else if(user)
+    res.redirect('/customer/home');
+  else
+    res.redirect('/login');
+}
+
+function registerNewUser(req, res) {
+  users.usersList.push(
+      {
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "email": req.body.email,
+        "password": req.body.password,
+        "id": (users.usersList.length + 1)
+      }
+  );
+  res.redirect('/login');
+}
+
+function createNewOffer(req, res) {
   console.log(req.body.localImg);
   offers.offers.push({
     "owner": req.body.owner,
@@ -118,14 +131,46 @@ app.post('/retailer/home', function (req, res) {
     "local-img": "/images/food-bread.jpg"
   });
   res.redirect('/customer/home');
-});
+};
 
-app.get('/retailer/info',retailerInfo.view);
-app.get('/retailer/options',retailerOptions.view);
-app.get('/retailer/verification',retailerVerification.view);
-app.get('/retailer/confirmation', retailerConfirmation.view);
+/*var url = 'mongodb://localhost:27017/myproject';
 
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+};
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs);
+    callback(docs);
+  });
+};
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  insertDocuments(db, function() {
+    findDocuments(db, function () {
+      db.close();
+    });
+  });
+});*/
+
